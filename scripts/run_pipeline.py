7fa30b65c9709810f98f2acd291d575fa14f7674
Quan <qqmquach@uwtaerloo.ca>
2026-04-05 16:19:19 -0400

    update run_pipeline to support and merge 2 sources before loading

--- scripts/run_pipeline.py
+++ scripts/run_pipeline.py
@@ -1,9 +1,16 @@
 from scripts.fetch_coingecko import fetch_coingecko_prices
+from scripts.fetch_kraken import fetch_kraken_prices
+from datetime import datetime, timezone
+
 from scripts.load import load_prices
 import logging 
 
 def main():
-    rows = fetch_coingecko_prices()
+    snapshot_time = datetime.now(timezone.utc).replace(microsecond=0)
+    cgk_rows = fetch_coingecko_prices(snapshot_time=snapshot_time)
+    kr_rows = fetch_kraken_prices(snapshot_time=snapshot_time)
+
+    rows = cgk_rows + kr_rows
     # logging.info(f"[FLOW] From [coingecko] Fetched {len(rows)} rows")
     load_prices(rows)
 
