8df0dc66e5f07d332750e7b9020939e69a5ac11c
Quan <qqmquach@uwtaerloo.ca>
2026-04-05 16:26:21 -0400

    update prefect flows

--- flows/prefect_flow.py
+++ flows/prefect_flow.py
@@ -1,13 +1,22 @@
 from prefect import flow, task
 import logging 
 from scripts.fetch_coingecko import fetch_coingecko_prices
+from scripts.fetch_kraken import fetch_kraken_prices
+
 from scripts.load import load_prices
 
 @task
-def fetch_task():
-    logging.info("[FLOW] Fetching data...")
+def fetch_coingecko_task():
+    logging.info("[FLOW] Fetching data from coingecko...")
     rows = fetch_coingecko_prices()
-    # logging.info(f"[FLOW] From [coingecko] Fetched {len(rows)} rows")
+    logging.info(f"[FLOW] From [coingecko] Fetched {len(rows)} rows")
+    return rows
+
+@task 
+def fetch_kraken_task():
+    logging.info("[FLOW] Fetching data from kraken...")
+    rows = fetch_kraken_prices()
+    logging.info(f"[FLOW] From [kraken] Fetched {len(rows)} rows")
     return rows
 
 @task
@@ -16,7 +25,9 @@ def load_task(rows):
 
 @flow(name="crypto-price-pipeline")
 def crypto_pipeline():
-    rows = fetch_task()
+    cgk_rows = fetch_coingecko_task()
+    kr_rows = fetch_coingecko_task()
+    rows = cgk_rows+kr_rows
     load_task(rows)
 
 if __name__ == "__main__":
